cb919e14cb0add65cdc9d9876c9e932368d1f690
alperen-kilic <alp.kilic.97@gmail.com>
2023-05-16 10:01:53 +0300

    loader change

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ClockLoader } from "react-spinners";
+import { RingLoader } from "react-spinners";
 
 const Loader = () => {
   return (
@@ -13,7 +13,7 @@ const Loader = () => {
       items-center 
     "
     >
-      <ClockLoader size={100} color="red" />
+      <RingLoader size={100} color="f43f5e" />
     </div>
   );
 };
